a1db59772ac7d0999b4e97f55ea094fb45990258
Thato Motau <ST10133742@vcconnect.edu.za>
2022-08-31 12:48:10 +0200

    2nd initial commit

---
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -0,0 +1,64 @@
+package com.example.calculator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun Plus(view: View) {
+        //does addition
+    }
+    fun Minus(view: View) {
+        //does subtraction
+    }
+    fun Multiply(view: View) {
+        //does multiplying
+    }
+    fun Divide(view: View) {
+        //does divide
+    }
+    fun Seven(view: View) {
+        //view the number seven
+    }
+    fun Eight(view: View) {
+        //view the number eight
+    }
+    fun Nine(view: View) {
+        //view the number nine
+    }
+    fun Clear(view: View) {
+        //does clear
+    }
+    fun Four(view: View) {
+        //view the number four
+    }
+    fun Five(view: View) {
+        //view the number five
+    }
+    fun Six(view: View) {
+        //view the number six
+    }
+    fun Zero(view: View) {
+        //view the number zero
+    }
+    fun One(view: View) {
+        //view the number one
+    }
+    fun Two(view: View) {
+        //view the number two
+    }
+    fun Three(view: View) {
+        //view the number three
+    }
+    fun Equal(view: View) {
+        //view the answer
+    }
+
+
+}
+
