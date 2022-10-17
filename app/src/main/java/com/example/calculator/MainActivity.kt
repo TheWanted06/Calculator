70f5ee96c40b6ddbd0beb2700417743903329d7f
Thato Motau <motaut2000@gmail.com>
2022-10-17 13:03:21 +0200

    All buttons display on the screen.

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -3,21 +3,23 @@ package com.example.calculator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    //, View.OnClickListener
+    val operations: List<String> = listOf("+","-","*","/","√","^","%");
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        //create int GLOBAL VARIABLES here > val
+        val operations: List<String> = listOf("+","-","*","/","√","^","%");
 
     }
 
-    fun SquareRoot(view: View) {
+    fun SquareRoot() {
         //does
-        val textViewMain = findViewById<TextView>(R.id.textViewMain) as TextView
-        val textView = findViewById<TextView>(R.id.textview) as TextView
+        val textViewMain = findViewById<TextView>(R.id.solution_tv) as TextView
+        val textView = findViewById<TextView>(R.id.result_tv) as TextView
         val oldtext = textViewMain.text;
         //val oldtextInt = Integer.parseInt(oldtext as String);
         //assign value to global variable
@@ -25,10 +27,10 @@ class MainActivity : AppCompatActivity() {
         textView.append(newtext)
         textViewMain.setText("")
     }
-    fun Power(view: View) {
+    fun Power() {
         //multiply by the exponent
-        val textViewMain = findViewById<TextView>(R.id.textViewMain) as TextView
-        val textView = findViewById<TextView>(R.id.textview) as TextView
+        val textViewMain = findViewById<TextView>(R.id.solution_tv) as TextView
+        val textView = findViewById<TextView>(R.id.result_tv) as TextView
         val oldtext = textViewMain.text;
         //val oldtextInt = Integer.parseInt(oldtext as String);
         //assign value to global variable
@@ -36,167 +38,144 @@ class MainActivity : AppCompatActivity() {
         textView.append(newtext)
         textViewMain.setText("");
     }
-    fun Delete(view: View) {
-        //does deleting the previous numbers entered
-
-        val textViewMain = findViewById<TextView>(R.id.textViewMain) as TextView
-        val textView = findViewById<TextView>(R.id.textview) as TextView
-        //assigning the textview numbers to variables
-        val oldtext1 = textViewMain.text;
-        val oldtext2 = textView.text;
-        var newtext = "";
-
-        //check if variable
+    fun BackSpace() {
+        val resutTV = findViewById<TextView>(R.id.result_tv) as TextView
+        val oldtext1 = resutTV.text;
         if(oldtext1.isNotEmpty()){
-            newtext = removeLastChars(oldtext1.toString(), 1)
-            textViewMain.text=newtext;
+            var newtext = removeLastChars(oldtext1.toString(), 1)
+            resutTV.text=newtext;
         }
-        else{
-            newtext = removeLastChars(oldtext2.toString(), 1)
-            textView.text=newtext;
-        }
-
     }
     fun removeLastChars(str: String, n: Int): String{
         return str.substring(0,str.length-n)
     }
+    fun Plus() {
+        val solutionTv = findViewById<TextView>(R.id.solution_tv) as TextView
+        val resultTV = findViewById<TextView>(R.id.result_tv) as TextView
+        val resultOldtext = resultTV.text.toString()
+        val solutionOldtext = solutionTv.text.toString()
 
-
-    fun Plus(view: View) {
-        //does addition
-        val textViewMain = findViewById<TextView>(R.id.textViewMain) as TextView
-        val textView = findViewById<TextView>(R.id.textview) as TextView
-        val oldtext = textViewMain.text;
-        //val oldtextInt = Integer.parseInt(oldtext as String);
-        //assign value to global variable
-        val newtext = "$oldtext + ";
-        textView.append(newtext)
-        textViewMain.setText("");
+        if(resultOldtext.isNotEmpty()){
+            val newtext = "$resultOldtext+";
+            solutionTv.append(newtext)
+            resultTV.setText("");
+        }else{
+            var newtext = removeLastChars(solutionOldtext.toString(), 1)
+            newtext += "+"
+            solutionTv.text=newtext;
+        }
     }
 
-    fun Minus(view: View) {
-        //does subtraction
-        val textViewMain = findViewById<TextView>(R.id.textViewMain) as TextView
-        val textView = findViewById<TextView>(R.id.textview) as TextView
-        val oldtext = textViewMain.text;
-        //val oldtextInt = Integer.parseInt(oldtext as String);
-        val newtext = "$oldtext - ";
-        textView.append(newtext)
-        textViewMain.setText("");
+    fun Minus() {
+        val solutionTv = findViewById<TextView>(R.id.solution_tv) as TextView
+        val resultTV = findViewById<TextView>(R.id.result_tv) as TextView
+        val resultOldtext = resultTV.text.toString()
+        val solutionOldtext = solutionTv.text.toString()
+
+        if(resultOldtext.isNotEmpty()){
+            val newtext = "$resultOldtext+";
+            solutionTv.append(newtext)
+            resultTV.setText("");
+        }else{
+            var newtext = removeLastChars(solutionOldtext.toString(), 1)
+            newtext += "-"
+            solutionTv.text=newtext;
+        }
     }
 
-    fun Multiply(view: View) {
-        //does multiplying
-        val textViewMain = findViewById<TextView>(R.id.textViewMain) as TextView
-        val textView = findViewById<TextView>(R.id.textview) as TextView
-        val oldtext = textViewMain.text;
-        //val oldtextInt = Integer.parseInt(oldtext as String);
-        val newtext = "$oldtext * ";
-        textView.append(newtext)
-        textViewMain.setText("");
+    fun Multiply() {
+        val solutionTv = findViewById<TextView>(R.id.solution_tv) as TextView
+        val resultTV = findViewById<TextView>(R.id.result_tv) as TextView
+        val resultOldtext = resultTV.text.toString()
+        val solutionOldtext = solutionTv.text.toString()
+
+        if(resultOldtext.isNotEmpty()){
+            val newtext = "$resultOldtext+";
+            solutionTv.append(newtext)
+            resultTV.setText("");
+        }else{
+            var newtext = removeLastChars(solutionOldtext.toString(), 1)
+            newtext += "*"
+            solutionTv.text=newtext;
+        }
     }
 
-    fun Divide(view: View) {
-        //does divide
-        val textViewMain = findViewById<TextView>(R.id.textViewMain) as TextView
-        val textView = findViewById<TextView>(R.id.textview) as TextView
-        val oldtext = textViewMain.text;
-        //val oldtextInt = Integer.parseInt(oldtext as String);
-        val newtext = "$oldtext / ";
-        textView.append(newtext)
-        textViewMain.setText("");
-    }
+    fun Divide() {
+        val solutionTv = findViewById<TextView>(R.id.solution_tv) as TextView
+        val resultTV = findViewById<TextView>(R.id.result_tv) as TextView
+        val resultOldtext = resultTV.text.toString()
+        val solutionOldtext = solutionTv.text.toString()
 
-    fun Seven(view: View) {
-        //view the number seven
-        val textViewMain = findViewById<TextView>(R.id.textViewMain) as TextView
-        textViewMain.append("7")
-    }
-
-    fun Eight(view: View) {
-        //view the number eight
-        val textViewMain = findViewById<TextView>(R.id.textViewMain) as TextView
-        textViewMain.append("8")
-    }
-
-    fun Nine(view: View) {
-        //view the number nine
-        val textViewMain = findViewById<TextView>(R.id.textViewMain) as TextView
-        textViewMain.append("9")
+        if(resultOldtext.isNotEmpty()){
+            val newtext = "$resultOldtext+";
+            solutionTv.append(newtext)
+            resultTV.setText("");
+        }else{
+            var newtext = removeLastChars(solutionOldtext.toString(), 1)
+            newtext += "/"
+            solutionTv.text=newtext;
+        }
     }
 
     fun Clear(view: View) {
-        //does clear
-        val textView = findViewById<TextView>(R.id.textview) as TextView
-        val textViewMain = findViewById<TextView>(R.id.textViewMain) as TextView
+        val textView = findViewById<TextView>(R.id.result_tv) as TextView
+        val textViewMain = findViewById<TextView>(R.id.solution_tv) as TextView
         textView.setText("");
         textViewMain.setText("");
     }
+    fun Dot(){
 
-    fun Four(view: View) {
-        //view the number four
-        val textViewMain = findViewById<TextView>(R.id.textViewMain) as TextView
-        textViewMain.append("4")
-    }
-
-    fun Five(view: View) {
-        //view the number five
-        val textViewMain = findViewById<TextView>(R.id.textViewMain) as TextView
-        textViewMain.append("5")
-    }
-
-    fun Six(view: View) {
-        //view the number six
-        val textViewMain = findViewById<TextView>(R.id.textViewMain) as TextView
-        textViewMain.append("6")
-    }
-
-    fun Zero(view: View) {
-        //view the number zero
-        val textViewMain = findViewById<TextView>(R.id.textViewMain) as TextView
-        textViewMain.append("0")
-    }
-
-    fun One(view: View) {
-        //view the number one
-    }
-
-    fun Two(view: View) {
-        //view the number two
-        val textViewMain = findViewById<TextView>(R.id.textViewMain) as TextView
-        textViewMain.append("2")
-    }
-
-    fun Three(view: View) {
-        //view the number three
-        val textViewMain = findViewById<TextView>(R.id.textViewMain) as TextView
-        textViewMain.append("3")
     }
 
     fun Equal(view: View) {
         //view the answer
-        val textView = findViewById<TextView>(R.id.textview) as TextView
+        val textView = findViewById<TextView>(R.id.result_tv) as TextView
 
     }
 
-    fun Point(view: View) {
-        //view in decimal number
-        val textViewMain = findViewById<TextView>(R.id.textViewMain) as TextView
-        textViewMain.append(".")
-    }
-    fun Open_Bracket(view: View){
-
-    }
-    fun Close_Bracket(view: View){
-
-    }
-    fun Reserve(view: View){
-
-    }
-    fun PnN(view: View){
+    fun Percent(){
 
     }
 
+    fun NumberEvent(view: View) {
+        val solutionTV = findViewById<TextView>(R.id.solution_tv) as TextView
+        val ResultTV = findViewById<TextView>(R.id.result_tv) as TextView
+
+        when(view.id){
+            //numbers
+            R.id.Btn1 -> {ResultTV.append("1")}
+            R.id.Btn2 -> {ResultTV.append("2")}
+            R.id.Btn3 -> {ResultTV.append("3")}
+            R.id.Btn4 -> {ResultTV.append("4")}
+            R.id.Btn5 -> {ResultTV.append("5")}
+            R.id.Btn6 -> {ResultTV.append("6")}
+            R.id.Btn7 -> {ResultTV.append("7")}
+            R.id.Btn8 -> {ResultTV.append("8")}
+            R.id.Btn9 -> {ResultTV.append("9")}
+            R.id.Btn0 -> {ResultTV.append("0")}
+            //other
+            R.id.open_bracket_Btn -> {ResultTV.append("(")}
+            R.id.close_bracket_Btn -> {ResultTV.append(")")}
+            R.id.Btn_Dot -> {ResultTV.append(".")}
+            R.id.PnN_Btn -> {Dot()}
+            R.id.backspace_Btn -> {BackSpace()}
+            //special symbols
+
+        }
+    }
+
+    fun OperationEvent(view: View) {
+        val ResultTV = findViewById<TextView>(R.id.result_tv) as TextView
+        when(view.id){
+            R.id.minus_Btn -> {Minus()}
+            R.id.plus_Btn -> {Plus()}
+            R.id.multiply_Btn -> {Multiply()}
+            R.id.divide_Btn -> {Divide()}
+            R.id.power_Btn -> {Power()}
+            R.id.SqRoot_Btn -> {SquareRoot()}
+            R.id.percent_Btn -> {Percent()}
+        }
+    }
 
 
 }
